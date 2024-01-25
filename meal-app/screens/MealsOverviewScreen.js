050e3b17a439b07f8fce469054dc19d2ca30ac0c
ojay234 <davidace998@gmail.com>
2024-01-25 22:34:17 +0100

    implemented mealItem component

--- meal-app/screens/MealsOverviewScreen.js
+++ meal-app/screens/MealsOverviewScreen.js
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummyData";
+import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item
+
+    const MealItemProps = {
+       title: item.title,
+       imageUrl: item.imageUrl,
+       affordability: item.affordability,
+       complexity: item.complexity,
+       duration: item.duration
+    }
+    
+    return <MealItem {...MealItemProps}/>
+  }
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
